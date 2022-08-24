bb3b7e292e16f22567891a9a5a4915d78afde896
Gede Gery Sastrawan <gerysastrawan123@gmail.com>
2022-08-24 14:49:27 +0800

    feat: add featureA

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func addFeature() {
+	fmt.Println("This is from featureA")
+}
+
 func main() {
 	fmt.Println("Hello Master!")
 	fmt.Println("Hello from Development!")
